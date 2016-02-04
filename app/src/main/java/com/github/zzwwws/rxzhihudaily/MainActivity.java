bcdfa5dbc9d9f2dca15dca0d9a8da35e31019737
zengzhiwen <zengzhiwen@yixin.im>
2016-02-04 16:59:14 +0800

    rm duplicate resource, fix toolbar

--- app/src/main/java/com/github/zzwwws/rxzhihudaily/MainActivity.java
+++ app/src/main/java/com/github/zzwwws/rxzhihudaily/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,23 +18,19 @@ import android.view.View;
  */
 public class MainActivity extends AppCompatActivity{
 
-    String TITLES[] = {"Home","Events","Mail","Shop","Travel"};
-    int ICONS[] = {R.drawable.ic_logo,R.drawable.ic_logo,R.drawable.ic_logo,R.drawable.ic_logo,R.drawable.ic_logo};
+    String TITLES[] = {"Home", "Events", "Mail", "Shop", "Travel"};
+    int ICONS[] = {R.drawable.ic_logo, R.drawable.ic_logo, R.drawable.ic_logo, R.drawable.ic_logo, R.drawable.ic_logo};
 
 
     String NAME = "Akash Bangad";
     String EMAIL = "akash.bangad@android4devs.com";
     int PROFILE = R.drawable.editor_profile_avatar;
-
-    private Toolbar toolbar;
-
     RecyclerView mRecyclerView;
     RecyclerView.Adapter mAdapter;
     RecyclerView.LayoutManager mLayoutManager;
     DrawerLayout Drawer;
-
     ActionBarDrawerToggle mDrawerToggle;
-
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +38,14 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
+
         setSupportActionBar(toolbar);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
 
         mRecyclerView.setHasFixedSize(true);
 
-        mAdapter = new MyAdapter(TITLES,ICONS,NAME,EMAIL,PROFILE);
+        mAdapter = new MyAdapter(TITLES, ICONS, NAME, EMAIL, PROFILE);
 
         mRecyclerView.setAdapter(mAdapter);
 
@@ -56,7 +55,7 @@ public class MainActivity extends AppCompatActivity{
 
 
         Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
-        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.open_drawer,R.string.close_drawer){
+        mDrawerToggle = new ActionBarDrawerToggle(this, Drawer, toolbar, R.string.open_drawer, R.string.close_drawer) {
 
             @Override
             public void onDrawerOpened(View drawerView) {
@@ -71,7 +70,6 @@ public class MainActivity extends AppCompatActivity{
             }
 
 
-
         };
         Drawer.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
@@ -79,21 +77,15 @@ public class MainActivity extends AppCompatActivity{
         toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -101,8 +93,10 @@ public class MainActivity extends AppCompatActivity{
     private Toolbar.OnMenuItemClickListener onMenuItemClickListener = new Toolbar.OnMenuItemClickListener() {
         @Override
         public boolean onMenuItemClick(MenuItem item) {
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
                 case R.id.message:
+                    break;
+                case R.id.action_search:
                     break;
                 case R.id.action_settings:
                     break;
